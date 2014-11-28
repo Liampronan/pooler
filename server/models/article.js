088358f029388aea1bca4137e172afad4d5d2ea5
Liam Ronan <liampronan@gmail.com>
2014-11-28 15:54:50 -0800

    remove article stats

--- server/models/article.js
+++ server/models/article.js
@@ -151,41 +151,41 @@ ArticleSchema.methods = {
  * Statics
  */
 
-ArticleSchema.statics = {
-
-  /**
-   * Find article by id
-   *
-   * @param {ObjectId} id
-   * @param {Function} cb
-   * @api private
-   */
-
-  load: function (id, cb) {
-    this.findOne({ _id : id })
-      .populate('user', 'name email username')
-      .populate('comments.user')
-      .exec(cb);
-  },
-
-  /**
-   * List articles
-   *
-   * @param {Object} options
-   * @param {Function} cb
-   * @api private
-   */
-
-  list: function (options, cb) {
-    var criteria = options.criteria || {}
-
-    this.find(criteria)
-      .populate('user', 'name username')
-      .sort({'createdAt': -1}) // sort by date
-      .limit(options.perPage)
-      .skip(options.perPage * options.page)
-      .exec(cb);
-  }
-}
+//ArticleSchema.statics = {
+//
+//  /**
+//   * Find article by id
+//   *
+//   * @param {ObjectId} id
+//   * @param {Function} cb
+//   * @api private
+//   */
+//
+//  load: function (id, cb) {
+//    this.findOne({ _id : id })
+//      .populate('user', 'name email username')
+//      .populate('comments.user')
+//      .exec(cb);
+//  },
+//
+//  /**
+//   * List articles
+//   *
+//   * @param {Object} options
+//   * @param {Function} cb
+//   * @api private
+//   */
+//
+//  list: function (options, cb) {
+//    var criteria = options.criteria || {}
+//
+//    this.find(criteria)
+//      .populate('user', 'name username')
+//      .sort({'createdAt': -1}) // sort by date
+//      .limit(options.perPage)
+//      .skip(options.perPage * options.page)
+//      .exec(cb);
+//  }
+//}
 
 mongoose.model('Article', ArticleSchema);
