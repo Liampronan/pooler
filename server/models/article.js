c5eff0e4469be1c3d07bb057701de4fe61cc5f6e
Liam Ronan <liampronan@gmail.com>
2014-11-28 16:59:29 -0800

    remove article stats

--- server/models/article.js
+++ server/models/article.js
@@ -151,32 +151,32 @@ ArticleSchema.methods = {
  * Statics
  */
 
-//ArticleSchema.statics = {
-//
-//  /**
-//   * Find article by id
-//   *
-//   * @param {ObjectId} id
-//   * @param {Function} cb
-//   * @api private
-//   */
-//
-//  load: function (id, cb) {
-//    this.findOne({ _id : id })
-//      .populate('user', 'name email username')
-//      .populate('comments.user')
-//      .exec(cb);
-//  },
-//
-//  /**
-//   * List articles
-//   *
-//   * @param {Object} options
-//   * @param {Function} cb
-//   * @api private
-//   */
-//
-//  list: function (options, cb) {
+ArticleSchema.statics = {
+
+  /**
+   * Find article by id
+   *
+   * @param {ObjectId} id
+   * @param {Function} cb
+   * @api private
+   */
+
+  load: function (id, cb) {
+    this.findOne({ _id : id })
+      .populate('user', 'name email username')
+      .populate('comments.user')
+      .exec(cb);
+  },
+
+  /**
+   * List articles
+   *
+   * @param {Object} options
+   * @param {Function} cb
+   * @api private
+   */
+
+  list: function (options, cb) {
 //    var criteria = options.criteria || {}
 //
 //    this.find(criteria)
@@ -185,7 +185,7 @@ ArticleSchema.methods = {
 //      .limit(options.perPage)
 //      .skip(options.perPage * options.page)
 //      .exec(cb);
-//  }
-//}
+  }
+}
 
 mongoose.model('Article', ArticleSchema);
