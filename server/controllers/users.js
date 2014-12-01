03e4c8e92a07b55c138014d4e862b2fe5c00c58f
Liam Ronan <liampronan@gmail.com>
2014-11-30 18:02:13 -0800

    no message

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -253,7 +253,7 @@ function login (req, res) {
   var redirectTo = req.session.returnTo ? req.session.returnTo : '/?uberId=' + req.user.uberid + '&firstName=' +
     req.user.firstName + '&profilePicture=' + req.user.profilePicture;
   delete req.session.returnTo;
-  res.redirect(redirectTo);
+  res.redirect("https://liampronan.com" + redirectTo);
 };
 
 
